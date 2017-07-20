197d3f48def1e57c7793011e7e878a9bbe3379fa
AMAIISDOM\bgarcia <BryanAnthony.Garcia@navitaire.com>
2017-07-21 00:59:44 +0800

    Add project files.

---
+++ PushNotifTrialService/Controllers/TodoItemController.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.OData;
+using Microsoft.Azure.Mobile.Server;
+using PushNotifTrialService.DataObjects;
+using PushNotifTrialService.Models;
+using Microsoft.Azure.Mobile.Server.Config;
+using Microsoft.Azure.NotificationHubs;
+using System.Collections.Generic;
+
+namespace PushNotifTrialService.Controllers
+{
+    public class TodoItemController : TableController<TodoItem>
+    {
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+            PushNotifTrialContext context = new PushNotifTrialContext();
+            DomainManager = new EntityDomainManager<TodoItem>(context, Request);
+        }
+
+        // GET tables/TodoItem
+        public IQueryable<TodoItem> GetAllTodoItems()
+        {
+            return Query();
+        }
+
+        // GET tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public SingleResult<TodoItem> GetTodoItem(string id)
+        {
+            return Lookup(id);
+        }
+
+        // PATCH tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task<TodoItem> PatchTodoItem(string id, Delta<TodoItem> patch)
+        {
+            return UpdateAsync(id, patch);
+        }
+
+        // POST tables/TodoItem
+        public async Task<IHttpActionResult> PostTodoItem(TodoItem item)
+        {
+            TodoItem current = await InsertAsync(item);
+
+            HttpConfiguration config = this.Configuration;
+            MobileAppSettingsDictionary settings = this.Configuration.GetMobileAppSettingsProvider().GetMobileAppSettings();
+
+            string notificationHubName = settings.NotificationHubName;
+
+            string notificationHubConnection = settings.Connections[MobileAppSettingsKeys.NotificationHubConnectionString].ConnectionString;
+
+            NotificationHubClient hub = NotificationHubClient.CreateClientFromConnectionString(notificationHubConnection, notificationHubName);
+
+            Dictionary<string, string> templateParams = new Dictionary<string, string>();
+            templateParams["messageParam"] = item.Text + " was added to the list";
+
+            try
+            {
+                var result = await hub.SendTemplateNotificationAsync(templateParams);
+
+                config.Services.GetTraceWriter().Info(result.State.ToString());
+            }
+            catch
+            {
+
+            }
+
+            return CreatedAtRoute("Tables", new { id = current.Id }, current);
+        }
+
+        // DELETE tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task DeleteTodoItem(string id)
+        {
+            return DeleteAsync(id);
+        }
+    }
+}
